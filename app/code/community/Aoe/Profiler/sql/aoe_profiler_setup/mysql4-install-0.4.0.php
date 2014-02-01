74ef7968a6887603aca6a54da719fb9e7b360b33
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2014-01-31 16:10:45 -0800

    Added database table

---
+++ app/code/community/Aoe/Profiler/sql/aoe_profiler_setup/mysql4-install-0.4.0.php
@@ -0,0 +1,29 @@
+<?php
+
+/* @var $this Mage_Core_Model_Resource_Setup */
+$this->startSetup();
+
+/**
+ * Create table 'aoe_profiler/profile'
+ */
+$table = $this->getConnection()
+    ->newTable($this->getTable('aoe_profiler/profile'))
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Profile Id')
+    ->addColumn('data', Varien_Db_Ddl_Table::TYPE_BLOB, null, array(
+    ), 'Data')
+    ->addColumn('route', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
+    ), 'Route')
+    ->addColumn('url', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
+    ), 'Url')
+    ->addColumn('total_time', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
+    ), 'Total Time')
+    ->addColumn('total_memory', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
+    ), 'Total Memory');
+$this->getConnection()->createTable($table);
+
+$this->endSetup();
