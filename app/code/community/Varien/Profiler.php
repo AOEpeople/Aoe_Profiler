23e87801c5120e2f4f1663b631637dce141fe959
Phillip Jackson <philwinkle@gmail.com>
2015-07-19 21:16:03 -0400

    conf->enableFilters also throwing notice for possibly undefined

--- app/code/community/Varien/Profiler.php
+++ app/code/community/Varien/Profiler.php
@@ -521,7 +521,7 @@ class Varien_Profiler
     {
         $conf = self::getConfiguration();
         $totals = self::getTotals();
-        return !$conf->enableFilters || (!$conf->filters->timeThreshold || $totals['time'] > $conf->filters->timeThreshold) &&
+        return empty($conf->enableFilters) || (!$conf->filters->timeThreshold || $totals['time'] > $conf->filters->timeThreshold) &&
                (!$conf->filters->memoryThreshold || $totals['realmem'] > $conf->filters->memoryThreshold);
     }
 
